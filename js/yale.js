71bfa25cec9871b9c80add9d172ed5ddbcb9fdea
mcowles-stack <mcowles@sas.upenn.edu>
2026-04-07 13:22:47 -0400

    Update yale.js

--- js/yale.js
+++ js/yale.js
@@ -1,1 +1,30 @@
-
+var tyler = {
+  "type": "FeatureCollection",
+  "features": [
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-72.9279, 41.3114] },
+      "properties": { "name": "Sterling Memorial Library" }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-72.9272, 41.3120] },
+      "properties": { "name": "Beinecke Rare Book Library" }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-72.9313, 41.3085] },
+      "properties": { "name": "Yale University Art Gallery" }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-72.9270, 41.3094] },
+      "properties": { "name": "Old Campus" }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-72.9234, 41.3199] },
+      "properties": { "name": "Payne Whitney Gymnasium" }
+    }
+  ]
+};
