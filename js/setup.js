ac9c882f4bc1b51df7023c2b2c7eedf0f45a086e
mcowles-stack <mcowles@sas.upenn.edu>
2026-04-07 13:22:04 -0400

    Update setup.js

--- js/setup.js
+++ js/setup.js
@@ -1,1 +1,5 @@
+var map = L.map('map').setView([41.3163, -72.9223], 15);
 
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
