51d231c3397215de8cce8dc94e274a186921f197
mcowles-stack <mcowles@sas.upenn.edu>
2026-04-07 15:47:45 -0400

    Update yale.js

--- js/yale.js
+++ js/yale.js
@@ -4,32 +4,32 @@ var tyler = {
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9279, 41.3114] },
-      "properties": { "name": "Sterling Memorial Library", "color": "blue" }
+      "properties": { "name": "Sterling Memorial Library" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9272, 41.3120] },
-      "properties": { "name": "Beinecke Rare Book Library", "color": "blue" }
+      "properties": { "name": "Beinecke Rare Book Library" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9313, 41.3085] },
-      "properties": { "name": "Yale University Art Gallery", "color": "blue" }
+      "properties": { "name": "Yale University Art Gallery" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9270, 41.3094] },
-      "properties": { "name": "Old Campus", "color": "blue" }
+      "properties": { "name": "Old Campus" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9249, 41.3149] },
-      "properties": { "name": "Benjamin Franklin College", "color": "red" }
+      "properties": { "name": "Benjamin Franklin College" }
     }
      {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9254, 41.3155] },
-      "properties": { "name": "Pauli Murray College", "color": "red" }
+      "properties": { "name": "Pauli Murray College" }
     }
   ]
 };
