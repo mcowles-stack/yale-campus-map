6ead75926e48ec47c1e01a412ea7ccf47bb9a18a
mcowles-stack <mcowles@sas.upenn.edu>
2026-04-07 21:23:18 -0400

    Update yale.js

--- js/yale.js
+++ js/yale.js
@@ -4,92 +4,92 @@ var tyler = {
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9300, 41.3113] },
-      "properties": { "name": "Sterling Memorial Library" }
+      "properties": { "name": "Sterling Memorial Library", "type": "academic" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9272, 41.3115] },
-      "properties": { "name": "Beinecke Rare Book Library" }
+      "properties": { "name": "Beinecke Rare Book Library", "type": "academic" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9313, 41.3085] },
-      "properties": { "name": "Yale University Art Gallery" }
+      "properties": { "name": "Yale University Art Gallery", "type": "arts" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9285, 41.3085] },
-      "properties": { "name": "Old Campus" }
+      "properties": { "name": "Old Campus", "type": "residential" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9249, 41.3149] },
-      "properties": { "name": "Benjamin Franklin College" }
+      "properties": { "name": "Benjamin Franklin College", "type": "residential" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9254, 41.3155] },
-      "properties": { "name": "Pauli Murray College" }
+      "properties": { "name": "Pauli Murray College", "type": "residential" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9254, 41.3110] },
-      "properties": { "name": "Silliman College" }
+      "properties": { "name": "Silliman College", "type": "residential" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9254, 41.3108] },
-      "properties": { "name": "Trumbull College" }
+      "properties": { "name": "Trumbull College", "type": "residential" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9272, 41.3099] },
-      "properties": { "name": "Grace Hopper College" }
+      "properties": { "name": "Grace Hopper College", "type": "residential" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9279, 41.3106] },
-      "properties": { "name": "Berkeley College" }
+      "properties": { "name": "Berkeley College", "type": "residential" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9298, 41.3098] },
-      "properties": { "name": "Branford College" }
+      "properties": { "name": "Branford College", "type": "residential" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9309, 41.3103] },
-      "properties": { "name": "Davenport College" }
+      "properties": { "name": "Davenport College", "type": "residential" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9306, 41.3125] },
-      "properties": { "name": "Ezra Stiles College" }
+      "properties": { "name": "Ezra Stiles College", "type": "residential" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9300, 41.3089] },
-      "properties": { "name": "Johnathan Edwards College" }
+      "properties": { "name": "Johnathan Edwards College", "type": "residential" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9305, 41.3126] },
-      "properties": { "name": "Morse College" }
+      "properties": { "name": "Morse College", "type": "residential" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9325, 41.3100] },
-      "properties": { "name": "Pierson College" }
+      "properties": { "name": "Pierson College", "type": "residential" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9294, 41.3102] },
-      "properties": { "name": "Saybrook College" }
+      "properties": { "name": "Saybrook College", "type": "residential" }
     },
     {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9233, 41.3105] },
-      "properties": { "name": "Timothy Dwight College" }
+      "properties": { "name": "Timothy Dwight College", "type": "residential" }
     }
   ]
 };
