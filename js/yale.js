8419946ff8f6eb92de0beafcc11b248d24b04fa4
mcowles-stack <mcowles@sas.upenn.edu>
2026-04-07 21:01:56 -0400

    Update yale.js

--- js/yale.js
+++ js/yale.js
@@ -40,6 +40,56 @@ var tyler = {
       "type": "Feature",
       "geometry": { "type": "Point", "coordinates": [-72.9254, 41.3108] },
       "properties": { "name": "Trumbull College" }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-72.9272, 41.3099] },
+      "properties": { "name": "Grace Hopper College" }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-72.9279, 41.3106] },
+      "properties": { "name": "Berkeley College" }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-72.9298, 41.3098] },
+      "properties": { "name": "Branford College" }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-72.9309, 41.3103] },
+      "properties": { "name": "Davenport College" }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-72.9306, 41.3125] },
+      "properties": { "name": "Ezra Stiles College" }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-72.9300, 41.3089] },
+      "properties": { "name": "Johnathan Edwards College" }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-72.9305, 41.3126] },
+      "properties": { "name": "Morse College" }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-72.9325, 41.3100] },
+      "properties": { "name": "Pierson College" }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-72.9294, 41.3102] },
+      "properties": { "name": "Saybrook College" }
+    },
+    {
+      "type": "Feature",
+      "geometry": { "type": "Point", "coordinates": [-72.9233, 41.3105] },
+      "properties": { "name": "Timothy Dwight College" }
     }
   ]
 };
