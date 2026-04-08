5f60698454dd591618fb4845b2233a38777acb6f
mcowles-stack <mcowles@sas.upenn.edu>
2026-04-07 22:59:15 -0400

    Update yale.js

--- js/yale.js
+++ js/yale.js
@@ -3,7 +3,7 @@ var tyler = {
   "features": [
     {
       "type": "Feature",
-      "geometry": { "type": "Point", "coordinates": [-72.9323, 41.3117] },
+      "geometry": { "type": "Point", "coordinates": [-72.9310, 41.3117] },
       "properties": { "name": "Sterling Memorial Library", "type": "academic" }
     },
     {
